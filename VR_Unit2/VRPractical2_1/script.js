0c4f0180cf2805f7029b3a56bcf5a2ed08fb91a8
Preston <prestonl13@nycstudents.net>
2025-10-17 02:02:44 +0000

    wdada

--- VR_Unit2/VRPractical2_1/script.js
+++ VR_Unit2/VRPractical2_1/script.js
@@ -43,20 +43,20 @@ function createCloud(x, y, z) {
   let cloud = document.createElement("a-entity");
 
   let puff1 = document.createElement("a-sphere");
-  puff1.setAttribute("color", "blue");
-  puff1.setAttribute("radius", 1.2);
+  puff1.setAttribute("color", "white");
+  puff1.setAttribute("radius", 1.6);
   puff1.setAttribute("position", { x: 0, y: 0.3, z: 0 });
   cloud.append(puff1);
 
   let puff2 = document.createElement("a-sphere");
-  puff2.setAttribute("color", "black");
-  puff2.setAttribute("radius", 1.5);
+  puff2.setAttribute("color", "white");
+  puff2.setAttribute("radius", 1.3);
   puff2.setAttribute("position", { x: 1.2, y: 0.4, z: -0.3 });
   cloud.append(puff2);
 
   let puff3 = document.createElement("a-sphere");
-  puff3.setAttribute("color", "green");
-  puff3.setAttribute("radius", 2);
+  puff3.setAttribute("color", "white");
+  puff3.setAttribute("radius", 1.1);
   puff3.setAttribute("position", { x: -1.1, y: 0.2, z: 0.2 });
   cloud.append(puff3);
 
