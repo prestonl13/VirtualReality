53a57ee47d3da44967a7ef6f50e9a1942de01b59
Preston <prestonl13@nycstudents.net>
2025-11-25 15:39:30 +0000

    wdadawd

--- VR_Unit3/VRPractical3_2/script.js
+++ VR_Unit3/VRPractical3_2/script.js
@@ -1,16 +1,17 @@
+let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene;
-
+let rocket;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene")
 
-
+  rocket = new Rocket(1, 3, 1);
       
   loop();
 })
 
 function loop(){
+  rocket.launch();
 
-    
   window.requestAnimationFrame(loop);
 }
