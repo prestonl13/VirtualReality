21d6a6267ab723f6337b88cd6590b693403dd538
Preston <prestonl13@nycstudents.net>
2025-11-26 21:50:10 +0000

    wdwadawdaw

--- VR_Unit3/VRPractical3_2/script.js
+++ VR_Unit3/VRPractical3_2/script.js
@@ -1,17 +1,25 @@
-let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene;
-let rocket;
+let scene, snowman;
+let snowman, snowman2, snowman3;
 
-window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene")
+window.addEventListener("DOMContentLoaded", function() {
+  scene = document.querySelector("a-scene");
+  snowmanTemplate = document.querySelector("#carTemplate");
 
-  rocket = new Rocket(1, 3, 1);
-      
+  snowman = new mySnowman(10, 1, -5); 
+  snowman2 = new mySnowman(-16, 1, -2); 
+  snowman3 = new mySnowman(7, 1 ,-20); 
+
+
   loop();
-})
+});
 
-function loop(){
-  rocket.launch();
+function loop() {
+
+  snowman.spin();
+  snowman2.spin();
+  snowman3.spin(); 
+
+
 
   window.requestAnimationFrame(loop);
 }
